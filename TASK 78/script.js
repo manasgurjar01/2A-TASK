4da070f1448e2daf8206d5be53b201b7cc0bc35a
manasgurjar01 <manasgurjar22@gmail.com>
2026-09-05 20:45:14 +0530

    add changes

--- TASK 78/script.js
+++ TASK 78/script.js
@@ -1,18 +1,13 @@
 let sort = document.querySelector("#sort");
-
 let products = document.querySelector("#products");
-
 let cards = document.querySelectorAll(".card");
 
 sort.addEventListener("change", function () {
   let cardsArray = Array.from(cards);
-
   if (sort.value == "low") {
     cardsArray.sort(function (a, b) {
       let priceA = Number(a.querySelector(".price").innerText);
-
       let priceB = Number(b.querySelector(".price").innerText);
-
       return priceA - priceB;
     });
   }
@@ -20,9 +15,7 @@ sort.addEventListener("change", function () {
   if (sort.value == "high") {
     cardsArray.sort(function (a, b) {
       let priceA = Number(a.querySelector(".price").innerText);
-
       let priceB = Number(b.querySelector(".price").innerText);
-
       return priceB - priceA;
     });
   }
