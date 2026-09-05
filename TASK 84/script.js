517b5b721e772643711526d7d964a07bd11faa05
manasgurjar01 <manasgurjar22@gmail.com>
2026-09-05 21:33:08 +0530

    add changes

--- TASK 84/script.js
+++ TASK 84/script.js
@@ -3,7 +3,6 @@ function getWeather() {
 
   fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + city)
     .then((response) => response.json())
-
     .then((data) => {
       let lat = data.results[0].latitude;
       let lon = data.results[0].longitude;
@@ -16,7 +15,6 @@ function getWeather() {
           "&current=temperature_2m",
       )
         .then((response) => response.json())
-
         .then((weather) => {
           document.querySelector("#result").innerText =
             "Temperature: " + weather.current.temperature_2m + "°C";
