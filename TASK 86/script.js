d1503746aa3c79589a6fa60aed536bbef99a6008
manasgurjar01 <manasgurjar22@gmail.com>
2026-09-05 21:38:49 +0530

    add changes

--- TASK 86/script.js
+++ TASK 86/script.js
@@ -1,19 +1,16 @@
 let search = document.querySelector("#search");
 let list = document.querySelector("#list");
-
 let users = [];
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
     users = data;
-
     showData(users);
   });
 
 function showData(arr) {
   list.innerHTML = "";
-
   arr.forEach((user) => {
     list.innerHTML += `
       <li>${user.name}</li>
@@ -23,10 +20,8 @@ function showData(arr) {
 
 search.addEventListener("input", () => {
   let text = search.value.toLowerCase();
-
   let result = users.filter((user) => {
     return user.name.toLowerCase().includes(text);
   });
-
   showData(result);
 });
