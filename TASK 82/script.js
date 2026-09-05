d5853b3b5b28481c566c7725c2ff01b601babd2e
manasgurjar01 <manasgurjar22@gmail.com>
2026-09-05 21:22:02 +0530

    add changes

--- TASK 82/script.js
+++ TASK 82/script.js
@@ -1,19 +1,15 @@
 let loader = document.querySelector("#loader");
 let users = document.querySelector("#users");
 
-// Loader show
 loader.style.display = "block";
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(function (response) {
     return response.json();
   })
-
   .then(function (data) {
-    // Data aa gaya, loader hide
     loader.style.display = "none";
 
-    // Users show
     data.forEach(function (user) {
       users.innerHTML += `
         <div class="user">
@@ -25,8 +21,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
   })
 
   .catch(function () {
-    // Error par loader hide
     loader.style.display = "none";
-
     users.innerHTML = "<p>Data load nahi ho paya</p>";
   });
