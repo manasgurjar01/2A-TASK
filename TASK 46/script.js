d95a27d9f847a64d5effd160a54ef7ea6a001290
manasgurjar01 <manasgurjar22@gmail.com>
2026-09-05 16:20:02 +0530

    add new changes

--- TASK 46/script.js
+++ TASK 46/script.js
@@ -1,22 +1,19 @@
 let btn = document.querySelector("#btn");
-let result = document.querySelector("h2");
+let result = document.querySelector("#result");
 
 btn.addEventListener("click", () => {
+
   let input = document.querySelector("#input").value;
 
-  let arrval = input.split(",");
-  // console.log(arrval);
+  let arr = input.split(",");
 
-  let numval = arrval.map((value) => {
+  let numbers = arr.map((value) => {
     return Number(value);
   });
-  // console.log(numval);
 
-  let sortval = numval.sort((a, b) => {
+  let sorted = numbers.sort((a, b) => {
     return a - b;
   });
 
-  console.log(sortval);
-
-  result.innerHTML = ` ${sortval}`;
+  result.innerHTML = sorted;
 });
